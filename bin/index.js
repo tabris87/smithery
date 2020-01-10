80c4d17c3266c0e29ef5c127a39cf2e6014e6a40
Adrian Marten <adrian.marten@msg.group>
2020-01-10 21:03:28 +0100

    Added some console outputs

--- bin/index.js
+++ bin/index.js
@@ -23,11 +23,12 @@ console.log(JSON.stringify(process.execArgv));
 const path = require('path');
 const fs = require('fs');
 
-console.log(path.join(process.argv[1].replace('index.js'), '../../../js2flowchart/src/builder/FlowTreeBuilder.js'));
-var bExists = fs.existsSync(path.join(process.argv[1].replace('index.js'), '../../../js2flowchart/src/builder/FlowTreeBuilder.js'));
+console.log(path.join(process.argv[1].replace('index.js'), '../../../js2flowchart'));
+var bExists = fs.existsSync(path.join(process.argv[1].replace('index.js'), '../../../js2flowchart'));
 console.log('Exists: ' + bExists);
 if (bExists) {
-    const oFlowTreeBuilder = require(path.join(process.argv[1].replace('index.js'), '../../../js2flowchart/src/builder/FlowTreeBuilder'));
+    const oModule = require(path.join(process.argv[1].replace('index.js'), '../../../js2flowchart'));
+    debugger;
 }
 
 cli.run(process);
