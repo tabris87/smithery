27c9687658515dfebdf9139493012ef277efb2ab
Adrian Marten <adrian.marten@msg.group>
2020-03-01 21:55:32 +0100

    Closes Issue #1

--- bin/api.js
+++ bin/api.js
@@ -6,5 +6,9 @@
 "use strict";
 
 module.exports = {
-    Project: require('../lib/Project');
+    Project: require('../lib/Project'),
+    ParserFactory: require('../lib/Parser'),
+    GeneratorFactory: require('../lib/Generator'),
+    RuleSetFactory: require('../lib/RuleSet'),
+    Imposer: require('../lib/Imposer')
 };
