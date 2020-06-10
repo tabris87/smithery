0d0dc1824072b36990647a3be980c854440c8e0c
Adrian Marten <Adrian.Marten@msg.group>
2020-06-10 10:38:44 +0200

    Fixed some issues and added some fixes for error handling and configuration applying

--- bin/cli.js
+++ bin/cli.js
@@ -9,38 +9,6 @@ const _setupOptions = function () {
     for (var key of Object.keys(commands)) {
         commands[key].createCommand(program);
     }
-    /* program
-        .command('init') // sub-command name
-        .alias('I')
-        .description('Inititalize the project setup') // command description
-
-        // function to execute when command is used
-        .action(function (args) {
-            command.init(args);
-        }); */
-
-    /*  program
-         .command('merge <base> <feature> [output]') // sub-command name
-         .alias('M')
-         .description('Merge two files given (base and feature file), by the default rules') // command description
-         .on('--help', function () {
-             console.log('\n', 'Examples:\n', '\n', '$fjs merge ./base.js ./feature.js\n', '$fjs merge ./base.js ./feature.js ./output.js');
-         })
-         // function to execute when command is used
-         .action(function (base, feature, output) {
-             command.merge(base, feature, output);
-         }); */
-
-    /* program
-        .command('build [configName]')
-        .alias('B')
-        .description('Starts the build of your product by the given configuration, otherwise tries to use the default config')
-        .on('--help', function () {
-            console.log('\n', 'Examples:\n', '\n', '$fjs build config1\n', '$fjs build config1')
-        })
-        .action(function(configName) {
-            command.
-        }); */
 }
 
 const run = function (oProcess) {
